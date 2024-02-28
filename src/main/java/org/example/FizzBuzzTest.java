f3bc7b4f03d057b04b4fc3d924aa05dc3e01b034
krish-0003 <gondaliyakrish0003@gmail.com>
2024-02-28 18:52:45 -0500

    Test: failing test for Fizz condition

---
+++ src/main/java/org/example/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package org.example;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizz() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3));//failing test:
+
+    }
+
+
+}
