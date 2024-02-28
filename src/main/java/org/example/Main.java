4e456aa52965d3da29da606003f186d51e0f32f5
krish-0003 <gondaliyakrish0003@gmail.com>
2024-02-28 18:44:58 -0500

    Intial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.example.FizzBuzz.*;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+
+    }
+}
